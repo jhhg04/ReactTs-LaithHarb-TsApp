1ae5260e22bd779e6e63adab9b85250054092e56
jhhg04 <jhhg04@gmail.com>
2022-04-27 22:19:35 -0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
-// import {useState} from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import List from './components/List';
 
 function App() {
-  // const [number, setNumber] = useState(5)
+  /*
+  const [number, setNumber] = useState(5)
+  const changeNumber=()=>{
+    setNumber(15)
+  }
+  */
 
-  // const changeNumber=()=>{
-  //   setNumber(15)
-  // }
+  interface IState {
+    people: {
+      name: string;
+      age: number;
+      url: string;
+      note?: string;
+    }[];
+  }
+
+  const [people, peopleSet] = useState<IState['people']>([
+    {
+      name: 'LeBron James',
+      age: 36,
+      url: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+      note: 'Allegeric to stay on the same team',
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>People Invited to my Party</h1>
+      <List people={people} />
     </div>
   );
 }
